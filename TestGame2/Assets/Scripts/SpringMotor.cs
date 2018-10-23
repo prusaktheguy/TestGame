852534f56ad6f3e68f0fb89ef53b7d59329bd268
prusaktheguy <prusaktheguy@o2.pl>
2018-10-23 05:41:13 +0200

    Spring Mechanics
    Applied spring mechanics ( moving platform, shooting player, and reloading for next shoot)

--- TestGame2/Assets/Scripts/SpringMotor.cs
+++ TestGame2/Assets/Scripts/SpringMotor.cs
@@ -5,22 +5,62 @@ using UnityEngine;
 public class SpringMotor : MonoBehaviour {
     public Rigidbody player;
     public float springForce;
+    // How much spring will go down after contact with player
+    public float springMovementRange;
+    // a variable for checking if first shoot happened and a need for cooldown before next shoot
+    bool isFirstTimeShooted=false;
+    //indicates that player should be shooted when spring reach lower limit
+    bool shootMe = false;
+    public float springMovementSpeed;
+    public float velocity;
+    float upperRangeLimit;
+    public float lowerRangeLimit;
+    Rigidbody m_Rigidbody;
+
 
     // Use this for initialization
     void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+        m_Rigidbody = GetComponent<Rigidbody>();
+        lowerRangeLimit = m_Rigidbody.position.y;
+        upperRangeLimit = lowerRangeLimit + springMovementRange;
+        m_Rigidbody.velocity = Vector3.zero;
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (m_Rigidbody.velocity!=Vector3.zero && m_Rigidbody.position.y >= upperRangeLimit )
+        {
+            m_Rigidbody.velocity = Vector3.zero;
+        }
+        if (m_Rigidbody.position.y < lowerRangeLimit && shootMe)
+        {
+            player.AddForce(Vector3.up * springForce);
+
+            m_Rigidbody.AddForce(Vector3.up * springMovementSpeed);
+            shootMe = false;
+        }
+        velocity = m_Rigidbody.velocity.y;
 	}
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == "Player")
         {
-            player.AddForce(Vector3.up*springForce);
 
+            if (!isFirstTimeShooted)
+            {
+                isFirstTimeShooted = true;
+                player.AddForce(Vector3.up * springForce);
+
+                m_Rigidbody.AddForce(Vector3.up * springMovementSpeed);
+
+            }
+            else
+            {
+                shootMe = true;
+
+            }
         }
     }
 
