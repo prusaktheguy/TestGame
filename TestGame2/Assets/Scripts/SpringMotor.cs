84f168ff72bf025003b658eecd82138b973d6d15
prusaktheguy <prusaktheguy@o2.pl>
2018-10-23 04:00:09 +0200

    Arena and Player with movement
    Player Character with movement from Unity Standard Assets And basic arena

---
+++ TestGame2/Assets/Scripts/SpringMotor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringMotor : MonoBehaviour {
+    public Rigidbody player;
+    public float springForce;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            player.AddForce(Vector3.up * springForce);
+        }
+    }
+
+}
